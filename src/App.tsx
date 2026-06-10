6eb050c8b1326a07223abe348b6fed98e5a134d2
Kishan Davda <kishandavda2008@gmail.com>
2026-06-10 11:23:24 +0530

    Fix SVG data syntax and asset URL resolution

--- src/App.tsx
+++ src/App.tsx
@@ -198,7 +198,7 @@ function App() {
                 <span className="underline underline-offset-1">hello: kishandavda2008@gmail.com</span>
                 <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                   <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
-                  <path d="M5 15H4a2 2 0 0 2 2 0 0 1 2 2v1"></path>
+                  <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
                 </svg>
               </button>
             </div>
